51ae79e1c1912fec0eb73bd9569e38fcbb32e55f
Brendan Molloy <brendan@technocreatives.com>
2022-02-11 11:10:30 +0100

    Add next/link wrapper for routes

--- packages/routing-next/src/index.tsx
+++ packages/routing-next/src/index.tsx
@@ -1,8 +1,27 @@
 import React from "react"
+import NextLink from "next/link"
 import { useRouter } from "next/router"
-import { fromUrl } from "@thetc/routing"
+import { fromUrl, toRelativeUrl } from "@thetc/routing"
 
 export function useRoute<T>(constructor: abstract new (...args: any[]) => T): T {
   const { asPath, route } = useRouter()
   return fromUrl(constructor, asPath, route.replace(/\[(.*?)\]/g, "{$1}"))
+}
+
+export type LinkProps<T> = {
+  route: T,
+  replace?: boolean;
+  scroll?: boolean;
+  shallow?: boolean;
+  passHref?: boolean;
+  prefetch?: boolean;
+  locale?: string | false;
+}
+
+export function Link<T>(props: React.PropsWithChildren<LinkProps<T>>) {
+  const ordinaryProps = props
+  const route = props.route
+  delete (ordinaryProps as any).route
+
+  return <NextLink {...ordinaryProps} href={toRelativeUrl(route)} />
 }
